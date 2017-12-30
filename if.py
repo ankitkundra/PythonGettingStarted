5c8f9a7b1dbd334141bfff0119786b9293bcd77b
ankitkundra <34942455+ankitkundra@users.noreply.github.com>
2017-12-30 21:57:04 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+num1 = input("Enter first number")
+num2 = input("Enter second number")
+
+if num1 > num2:
+    print("First number is greater than second number")
+elif num2 > num1:
+    print("Second number is greater than first number")
+else:
+    print("Both numbers are equal")
+    
